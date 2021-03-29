dce757f37ce5b47d220a61267de7c2cc06f0d2ff
unknown <jbj1996@gmail.com>
2021-03-29 18:20:23 -0400

    created native product variable to store key info from site

--- myapp/management/commands/scrape_shopify.py
+++ myapp/management/commands/scrape_shopify.py
@@ -11,13 +11,27 @@ class Command(BaseCommand):
         store_url = options['store_url'].pop()
 
         # Collections
-        collections_url = f"{store_url}/collections.json?limit=250"
-        r = requests.get(collections_url)
-        collections = r.json()["collections"]
+        # collections_url = f"{store_url}/collections.json?limit=250"
+        # r = requests.get(collections_url)
+        # collections = r.json()["collections"]
 
         # Products
         products_url = f"{store_url}/products.json?limit=250"
         r = requests.get(products_url)
-        products = r.json()["products"]
+        foreign_products = r.json()["products"]
 
-        print(products.pop())
+        foreign_product = foreign_products.pop()
+        
+        native_product = {
+            "title": None,
+            "handle": None,
+            "description": None,
+            "collection": None,
+        }
+        
+        
+        native_product["handle"] = foreign_product["handle"]
+        native_product["description"] = foreign_product["body_html"]
+        native_product["title"] = foreign_product["title"]
+        
+        print(native_product)
