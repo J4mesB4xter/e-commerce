9a8b259dd0f9767fbff697cd09f96cece6207218
unknown <jbj1996@gmail.com>
2021-04-06 17:26:50 -0400

    removed unnecessary imports

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,6 @@
-from django.core.files import File
-from django.db import models
 from django_instant_rest.models import RestResource, RestClient
+from django.db import models
 from mysite import settings
-from urllib.request import urlretrieve
-import os
 
 class Collection(RestResource):
     title = models.CharField(max_length = 255)
@@ -28,7 +25,7 @@ class CollectionMedia(RestResource):
 class ProductMedia(RestResource):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     url = models.CharField(max_length = 1023)
-    
+
 class VariantMedia(RestResource):
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
     url = models.CharField(max_length = 1023)
