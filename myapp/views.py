c14c1c3207e22ccbb29e9ebdc5a2238bafa7adf0
unknown <jbj1996@gmail.com>
2021-04-06 17:50:29 -0400

    added initial templates folder and view

--- myapp/views.py
+++ myapp/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from myapp import models
+from django.http import HttpResponse
 
+def product_page(request):
+    template = loader.get_template("product.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from myapp.models import *
 from django_instant_rest import patterns
+from django.urls import path
+from myapp import views
 
 urlpatterns = [
     patterns.resource('collections.json', Collection),
     patterns.resource('products.json', Product),
     patterns.resource('variants.json', Variant),
-]
+    path('product/', views.product_page),
+    ]
