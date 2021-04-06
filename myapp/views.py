9b191f8f557e2648ac14fcc6a16c494e1868c19e
unknown <jbj1996@gmail.com>
2021-04-06 18:11:42 -0400

    added 404 handling for product view

--- myapp/views.py
+++ myapp/views.py
@@ -4,9 +4,14 @@ from myapp import models
 from django.http import HttpResponse
 
 def product_page(request, handle):
-    product = models.Product.objects.get(handle=handle)
-    template = loader.get_template("product.html")
-    context = {
-        "product" : product
-    }
-    return HttpResponse(template.render(context, request))
+    try:
+        product = models.Product.objects.get(handle=handle)
+        template = loader.get_template("product.html")
+        context = {
+            "product" : product
+        }
+        return HttpResponse(template.render(context, request))
+    except:
+        template = loader.get_template("404.html")
+        return HttpResponse(template.render({}, request))
+        
