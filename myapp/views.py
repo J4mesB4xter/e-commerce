89d6c1813948174d8601ce82f48d86950df58128
unknown <jbj1996@gmail.com>
2021-04-13 13:23:19 -0400

    added image to product.html... incorrect image, but an image is present

--- myapp/views.py
+++ myapp/views.py
@@ -4,14 +4,16 @@ from myapp import models
 from django.http import HttpResponse
 
 def product_page(request, handle):
-    try:
-        product = models.Product.objects.get(handle=handle)
-        template = loader.get_template("product.html")
-        context = {
-            "product" : product
-        }
-        return HttpResponse(template.render(context, request))
-    except:
-        template = loader.get_template("404.html")
-        return HttpResponse(template.render({}, request))
+    # try:
+    product = models.Product.objects.get(handle=handle)
+    image = models.ProductMedia.objects.select_related("product")[product.id-1].url
+    template = loader.get_template("product.html")
+    context = {
+        "product" : product,
+        "image" : image
+    }
+    return HttpResponse(template.render(context, request))
+    # except:
+    #     template = loader.get_template("404.html")
+    #     return HttpResponse(template.render({}, request))
         
