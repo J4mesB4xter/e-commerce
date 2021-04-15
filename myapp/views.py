951ff60193894dac94155c23446af092ecea7b2a
unknown <jbj1996@gmail.com>
2021-04-14 20:01:46 -0400

    added price to product view

--- myapp/views.py
+++ myapp/views.py
@@ -9,17 +9,22 @@ def product_page(request, handle):
         title_prefix = product.title.split(" - ")[0]
         title_suffix = product.title.split(" - ")[1]
         images = models.ProductMedia.objects.filter(product=product)
+        variants = models.Variant.objects.filter(product=product)
+        price = "$" + "{:.2f}".format(variants[0].price_in_cents/100)
         related_products = models.Product.objects.filter(title__startswith=title_prefix)
         template = loader.get_template("product.html")
         context = {
             "product" : product,
             "images" : images,
+            "variants" : variants,
             "title_prefix" : title_prefix,
             "title_suffix" : title_suffix,
             "related_products" : related_products,
+            "price" : price
         }
         return HttpResponse(template.render(context, request))
-    except:
+    except Exception as e:
+        raise e
         template = loader.get_template("404.html")
         return HttpResponse(template.render({}, request))
         
